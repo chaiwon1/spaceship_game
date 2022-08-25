ae3def8d217eac58fccf9f22402c5e8be3ad15c7
현채원 <chaiwonhyun@hyeonchaewon-ui-MacBookAir.local>
2022-08-25 12:44:32 +0900

    done

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 // canvas setting
 let canvas;
 let ctx;
-
-canvas = document.createElement("canvas")
-ctx = canvas.getContext("2d")
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
 
 
 // canvas size
 canvas.width = 400;
 canvas.height = 700;
-document.body.appendChild(canvas)
-
+document.body.appendChild(canvas);
 
 // variable setting
 let backgroundImage, spaceshipImage, enemyImage, bulletImage, gameOverImage;
+let gameOver = false;
+
+// score
+let score = 0;
 
 // spaceship coordinate
-let spaceshipX = canvas.width/2 - 32
-let spaceshipY = canvas.height - 64
+let spaceshipX = canvas.width/2 - 32;
+let spaceshipY = canvas.height - 64;
+
 
 // bullet class
 let bulletList = []
@@ -25,15 +28,56 @@ function Bullet() {
     this.x = 0;
     this.y = 0;
     this.init = function() {
-        this.x = spaceshipX + 20
-        this.y = spaceshipY
-
-        bulletList.push(this)
-    }
+        this.x = spaceshipX + 20;
+        this.y = spaceshipY;
+        this.alive = true
+        bulletList.push(this);
+    };
     this.update = function() {
         this.y -= 5;
+    };
+    this.checkHit = function() {
+        for (let i=0;i<enemyList.length;i++) {
+            if (
+                this.y <= enemyList[i].y && 
+                this.x >= enemyList[i].x && 
+                this.x <= enemyList[i].x + 40
+                ) {
+                score++;
+                this.alive = false;
+                enemyList.splice(i, 1);
+            }
+        }
     }
 }
+
+
+// Random number generator
+function generateRandomValue(min, max) {
+    let randomNum = Math.floor(Math.random()*(max-min+1))+min;
+    return randomNum;
+}
+
+
+// enemy class
+let enemyList = []
+function Enemy() {
+    this.x = 0;
+    this.y = 0;
+    this.init = function() {
+        this.y = 0;
+        this.x = generateRandomValue(0, canvas.width-48);
+        enemyList.push(this);
+    };
+    this.update = function() {
+        this.y += 3; // 적군의 속도 조절
+
+        if (this.y >= canvas.height - 48) {
+            gameOver = true;
+        }
+    };
+}
+
 
 // load images
 function loadImage() {
@@ -50,17 +94,18 @@ function loadImage() {
     bulletImage.src = "images/bullet.png";
 
     gameOverImage = new Image();
-    gameOverImage.src = "images/gameover.jpeg";  
+    gameOverImage.src = "images/gameover.png";  
 }
+
 
 // keyboard setting
 let keysDown =  {}
 function setupKeyboardListener() {
     document.addEventListener("keydown", function(event){
-        keysDown[event.key] = true
+        keysDown[event.key] = true;
     });
     document.addEventListener("keyup", function(event){
-        delete keysDown[event.key]
+        delete keysDown[event.key];
         
         if (event.key == " ") {
             createBullet();
@@ -78,6 +123,17 @@ function setupKeyboardListener() {
 function createBullet() {
     let b = new Bullet();
     b.init();
+}
+
+// enemy fuction
+// 1. 1초마다 적군이 생긴다.
+// 2. 적군이 바닥에 닿으면 게임 오버
+// 3. 적군과 총알이 만나면 적군 사라지고 1점 획득
+function createEnemy() {
+    const interval = setInterval(function(){
+        let e = new Enemy();
+        e.init();
+    }, 1000)
 }
 
 // update coordinate
@@ -112,7 +168,15 @@ function update() {
 
     // 총알 업데이트
     for (let i=0;i<bulletList.length;i++) {
-        bulletList[i].update()
+        if (bulletList[i].alive) {
+            bulletList[i].update();
+            bulletList[i].checkHit()
+        }
+    }
+
+    // 적군 업데이트
+    for (let i=0;i<enemyList.length;i++) {
+        enemyList[i].update();
     }
 }
 
@@ -120,18 +184,33 @@ function update() {
 function render() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+    ctx.fillText(`Score:${score}`, 20, 20);
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
 
     for(let i=0;i<bulletList.length;i++) {
-        ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y)
+        if (bulletList[i].alive) {
+            ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y);
+        }
+    }
+
+    for(let i=0;i<enemyList.length;i++) {
+        ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y);
     }
 }
 
 function main() {
-    update();
-    render();
-    requestAnimationFrame(main);
+    if (!gameOver) {
+        update();
+        render();
+        requestAnimationFrame(main);
+    }
+    else{
+        ctx.drawImage(gameOverImage, 50, 100, 300, 350)
+    }
 }
 
 loadImage()
 setupKeyboardListener()
+createEnemy()
 main()
